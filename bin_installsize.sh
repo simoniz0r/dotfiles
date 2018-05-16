00165d144647db1010f231c2e4d9cee788959d93
simoniz0r <bnu.fantasma@gmail.com>
2018-05-16 14:25:56 -0500

    Add size per dir

--- bin_installsize.sh
+++ bin_installsize.sh
@@ -3,11 +3,13 @@
 TOTAL_SIZE=0
 for directory in $(dir -Cw1 / | grep -v 'proc' | grep -v 'tmp' | grep -v 'run' | grep -v 'home' | grep -v 'media' | grep -v 'snap' | grep -v 'var' | grep -v 'swapfile'); do
     DIR_SIZE=$(sudo du -b --max-depth=0 "/$directory" | cut -f1 -d'/' | tr -d '[:blank:]')
+    echo "$directory - $DIR_SIZE bytes"
     TOTAL_SIZE=$(($TOTAL_SIZE+$DIR_SIZE))
 done
 KB_SIZE=$(($TOTAL_SIZE/1024))
 MB_SIZE=$(($KB_SIZE/1024))
 GB_SIZE=$(($MB_SIZE/1024))
+echo
 printf '%s\n' "Total Install Size:
 Bytes:       $TOTAL_SIZE
 KiBs:        $KB_SIZE
